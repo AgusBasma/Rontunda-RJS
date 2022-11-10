c8a37219c9e3a3a77d7dd76d7bc5767b6058aeb3
AgusBasma <agus.basma97@gmail.com>
2022-11-09 23:43:14 -0300

    PreEntrega1-Basma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo512.png';
+import Header from './components/header';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+
+      <Navbar>
+        <CartWidget />
+      </Navbar>
+      <ItemListContainer />
+{     /*<header className="App-header"> /*
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +26,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header>*/ }
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+function Navbar () {
+    return (
+        <nav>
+            <ul class="enlaces">
+                <li class="enlaces-item">
+                    <a href="./index.html" >Inicio</a>
+                </li>
+                <li class="enlaces-item">
+                    <a href="./preguntas frecuentes.html" >Preguntas frecuentes</a>
+                </li>
+                <li class="enlaces-item">
+                    <a href="./e-shop.html" >E-shop</a>
+                </li>
+                <li class="enlaces-item">
+                    <a href="./contacto.html" >Contacto</a>
+                </li> 
+                <li class="enlaces-item">
+                    <a href="./galeria.html" >Galeria</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
